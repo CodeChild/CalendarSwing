0bc2f42dbbe66238749b0c671287c81674132b48
zhangyulong <zhangyulong@fenrir-tec.com>
2019-11-18 09:30:47 +0800

    Add set calendar date in model

--- src/com/zhangyulong/CalendarModel.java
+++ src/com/zhangyulong/CalendarModel.java
@@ -1,6 +1,5 @@
 package com.zhangyulong;
 
-import java.text.NumberFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -13,29 +12,39 @@ public class CalendarModel {
     private final int YEAR_LENGTH = 4;
     private Date[][] calendarDate = new Date[ROWS][COLUMNS];
     private SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd");
-    private NumberFormat yearFormat = NumberFormat.getInstance();
-    private NumberFormat monthDayFormat = NumberFormat.getInstance();
 
     CalendarModel() {
-        yearFormat.setMaximumIntegerDigits(YEAR_LENGTH);
-        monthDayFormat.setMaximumIntegerDigits(DAY_MONTH_LENGTH);
     }
 
     public void setCalendarDate(int year, int month) {
         int startOffset = 0;
+        String yearMonth = String.format("%04d", year) +
+                String.format("%02d", month);
         Calendar calendar = Calendar.getInstance();
+        calendar.setFirstDayOfWeek(Calendar.SUNDAY);
         try {
             calendar.setTime(simpleDateFormat.parse(
-                    yearFormat.format(year) +
-                            monthDayFormat.format(month) +
-                            monthDayFormat.format(1)));
+                    yearMonth +
+                            String.format("%02d", 1)));
+            startOffset = calendar.get(Calendar.DAY_OF_WEEK) - 1;
         } catch (ParseException e) {
             e.printStackTrace();
         }
         for (int row = 0; row < calendarDate.length; row++) {
             for (int column = 0; column < calendarDate[0].length; column++) {
-                int day = row - startOffset + 1;
+                int day = row * calendarDate[0].length + column + 1 - startOffset;
+                if (day > 0 && day <= 31) {
+                    try {
+                        calendarDate[row][column] = simpleDateFormat.parse(yearMonth + String.format("%02d", day));
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
         }
+    }
+
+    public Date[][] getCalendarDate() {
+        return calendarDate;
     }
 }
--- src/com/zhangyulong/Main.java
+++ src/com/zhangyulong/Main.java
@@ -1,8 +1,17 @@
 package com.zhangyulong;
 
+import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
+        CalendarModel calendarModel = new CalendarModel();
+        calendarModel.setCalendarDate(2010, 7);
+        for (Date[] i : calendarModel.getCalendarDate()) {
+            for (Date j : i) {
+                System.out.println(j);
+            }
+        }
     }
 }
